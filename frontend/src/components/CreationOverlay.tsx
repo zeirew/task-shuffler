11c88eccd1899db27ef99f28c0b4472e40afb96e
zeirew <23731719+zeirew@users.noreply.github.com>
2024-09-05 21:39:09 -0400

    Add x-out button

--- frontend/src/components/CreationOverlay.tsx
+++ frontend/src/components/CreationOverlay.tsx
@@ -1,16 +1,23 @@
 import './CreationOverlay.css';
 
-function CreationOverlay({ onClose }) {
+function CreationOverlay({onClose}) {
     return (
        <div className="overlay">
         <div className="overlay-content">
-            <button className="button-container" type="submit">X</button>
+            <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={onClose}>
+                <span aria-hidden="true">&times;</span>
+              </button>
             <form id="createForm" className="form-container">
                 <label htmlFor="title">Title:</label>
                 <input type="text" id="title" name="title" required/> 
             
                 <label htmlFor="description">Description:</label>
-                <textarea id="description" name="description"></textarea>
+                <textarea id="description" name="description" className="description-text-area"></textarea>
 
                 <button type="submit">Submit</button>
             </form>
